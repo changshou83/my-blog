10eb0b444c813ae0c7846cdf978b0f34139b6a20
changshou83 <1369548156@qq.com>
2022-04-16 23:02:19 +0800

    📃 docs(essay): add CSBasicMOOC

--- docs/.vitepress/content/essay.js
+++ docs/.vitepress/content/essay.js
@@ -3,6 +3,10 @@ const essaySideBar = [
     text: 'Windows下美化命令行',
     link: '/essay/美化PowerShell',
   },
+  {
+    text: '计算机基础知识相关推荐课程',
+    link: '/essay/计算机基础知识相关推荐课程',
+  },
 ];
 
 export { essaySideBar };
