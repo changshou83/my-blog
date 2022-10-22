9b7b12bcd0b066a8c3ed9f231dab537458e220ac
changshou83 <1369548156@qq.com>
2022-10-22 13:42:04 +0800

    docs(essay): 动态导入图片

--- docs/.vitepress/content/essay.js
+++ docs/.vitepress/content/essay.js
@@ -1,27 +1,31 @@
 const essaySideBar = [
   {
-    text: 'Windows下美化命令行',
-    link: '/essay/美化PowerShell',
+    text: "Windows下美化命令行",
+    link: "/essay/美化PowerShell",
   },
   {
-    text: '计算机基础知识相关推荐课程',
-    link: '/essay/计算机基础知识相关推荐课程',
+    text: "计算机基础知识相关推荐课程",
+    link: "/essay/计算机基础知识相关推荐课程",
   },
   {
-    text: 'JavaScript函数式编程(RxJS入门)',
-    link: '/essay/JavaScript函数式编程(RxJS入门)',
+    text: "JavaScript函数式编程(RxJS入门)",
+    link: "/essay/JavaScript函数式编程(RxJS入门)",
   },
   {
-    text: '各种linter',
-    link: '/essay/各种linter',
+    text: "各种linter",
+    link: "/essay/各种linter",
   },
   {
-    text: 'tao of node',
-    link: '/essay/tao-of-node',
+    text: "tao of node",
+    link: "/essay/tao-of-node",
   },
   {
-    text: 'JS & Node 的最佳测试实践',
-    link: '/essay/Node的最佳测试实践',
+    text: "JS & Node 的最佳测试实践",
+    link: "/essay/Node的最佳测试实践",
+  },
+  {
+    text: "如何在浏览器环境下动态导入图片",
+    link: "/essay/如何在浏览器环境下动态导入图片",
   },
 ];
 
